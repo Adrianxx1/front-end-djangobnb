8ea218d9f616bb5b96b1b4471d4f9198dfd70294
adrianxx1 <triaxis12q@gmail.com>
2025-04-04 10:10:41 -0600

    login

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+// import Modal from "./components/modals/Modal";
+import LoginModal from "./components/modals/LoginModal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,8 @@ export default function RootLayout({
       >
         <Navbar />
         <div className="pt-24">{children}</div>
+        {/* <Modal label="Modal Test" content={content} isOpen={true} /> */}
+        <LoginModal />
       </body>
     </html>
   );
